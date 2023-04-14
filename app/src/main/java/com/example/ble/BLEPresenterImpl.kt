07912cd6eba07c397db71c48ae5791e5eb12e0e6
takashi <takashiho0913@gmail.com>
2023-04-14 22:32:27 +0900

    [fix]不必要なコードを削除

--- app/src/main/java/com/example/ble/BLEPresenterImpl.kt
+++
@@ -1,7 +0,0 @@
-package com.example.ble
-
-import android.bluetooth.BluetoothGattCharacteristic
-
-class BLEPresenterImpl {
-
-}
--- app/src/main/java/com/example/ble/BleContents.kt
+++
@@ -1,6 +0,0 @@
-package com.example.ble
-
-data class BleContents(
-    val name : String,
-    val address : String
-)
